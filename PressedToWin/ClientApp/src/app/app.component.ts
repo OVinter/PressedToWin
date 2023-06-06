01999f0050bcaad6d92578b8e2089e5baedcc7a7
Mihai Tir <mihai.tir@cpp.canon>
2023-06-06 14:12:29 +0300

    Commit work

--- PressedToWin/ClientApp/src/app/app.component.ts
+++ PressedToWin/ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { SessionManagementService } from '@canonpp/duif-core';
+import { SessionManagementProvider } from '@canonpp/duif-core';
 
 
 @Component({
@@ -8,4 +10,19 @@ import { Component} from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(sessionManagment: SessionManagementService, private readonly sessionManagementProvider: SessionManagementProvider) {
+
+
+    this.sessionManagementProvider.authenticated$.subscribe(() => {
+      console.log(`Access token: ${this.sessionManagementProvider.getAccessTokens(null)[0]}`);
+    });
+
+    console.log("test")
+    // logs info about the currently logged in user
+   /* sessionManagment.userInfo$.subscribe(userInfo => {
+      console.log(userInfo);
+    });*/
+  }
+
+  title = 'app';
 }
